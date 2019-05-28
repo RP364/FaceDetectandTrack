f10ff43283b4cbedbd9c60167183cda2e74ca1b6
HBvision <dragonquest2019@gmail.com>
2019-05-28 10:21:33 -0400

    first commit

--- demo.py
+++ demo.py
@@ -3,13 +3,28 @@ import cv2
 import dlib
 import numpy as np
 import argparse
+import tensorflow
 from contextlib import contextmanager
 from wide_resnet import WideResNet
 from keras.utils.data_utils import get_file
+import os
 
-pretrained_model = "https://github.com/yu4u/age-gender-estimation/releases/download/v0.5/weights.28-3.73.hdf5"
-modhash = 'fbe63257a054c1c5466cfd7bf14646d6'
+#modelFile = "../data/face_detector/deploy.protxt"
+#configFile = "../data/face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+#model = cv2.dnn.readNetFromCaffe(modelFile, configFile)
 
+#pretrained_model = "https://github.com/yu4u/age-gender-estimation/releases/download/v0.5/weights.28-3.73.hdf5"
+#modhash = 'fbe63257a054c1c5466cfd7bf14646d6'
+
+IMAGE_DIR = '/home/r/deep_sort/'
+
+#cap = cv2.VideoCapture(os.path.join(IMAGE_DIR, 'test.mp4'))
+img = cv2.imread(os.path.join(IMAGE_DIR, 'test.png')) 
+#frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
+
+#fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+#out = cv2.VideoWriter(os.path.join(IMAGE_DIR, 'age_test.avi'), fourcc, 10, (frame_width, frame_height))
 
 def get_args():
     parser = argparse.ArgumentParser(description="This script detects faces from web cam input, "
@@ -37,25 +52,27 @@ def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
     cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness, lineType=cv2.LINE_AA)
 
 
+
 @contextmanager
-def video_capture(*args, **kwargs):
-    cap = cv2.VideoCapture(*args, **kwargs)
+def video_capture():
+
     try:
         yield cap
     finally:
         cap.release()
 
 
+
 def yield_images():
     # capture video
-    with video_capture(0) as cap:
+    with video_capture() as cap:
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
+        
         while True:
             # get video frame
             ret, img = cap.read()
-
+            
             if not ret:
                 raise RuntimeError("Failed to capture image")
 
@@ -76,12 +93,11 @@ def yield_images_from_dir(image_dir):
 
 def main():
     args = get_args()
-    depth = args.depth
+    depth = 16
     k = args.width
     weight_file = args.weight_file
     margin = args.margin
     image_dir = args.image_dir
-
     if not weight_file:
         weight_file = get_file("weights.28-3.73.hdf5", pretrained_model, cache_subdir="pretrained_models",
                                file_hash=modhash, cache_dir=str(Path(__file__).resolve().parent))
@@ -94,44 +110,53 @@ def main():
     model = WideResNet(img_size, depth=depth, k=k)()
     model.load_weights(weight_file)
 
-    image_generator = yield_images_from_dir(image_dir) if image_dir else yield_images()
+    #image_generator = yield_images_from_dir(image_dir) if image_dir else yield_images()
 
-    for img in image_generator:
-        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_h, img_w, _ = np.shape(input_img)
-
+    #for img in image_generator:
+    input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_h, img_w, _ = np.shape(input_img)
+    cv2.imshow("hey", img)
+    #cv2.waitkey()
         # detect faces using dlib detector
-        detected = detector(input_img, 1)
-        faces = np.empty((len(detected), img_size, img_size, 3))
+    #detected = detector(input_img, 1)
+    #h,w = img.shape[0:2]
+    #blob = cv2.dnn.blobFromImage((image, 1,  (300*w//h, 300)), (144, 177, 123), False)
+    #model.setInput(blob)
+    #detected = model.forward
+    detected_face = cv2.CascadeClassifier(img)
+    print(detected)
+    faces = np.empty((len(detected), img_size, img_size, 3))
 
-        if len(detected) > 0:
-            for i, d in enumerate(detected):
-                x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
-                xw1 = max(int(x1 - margin * w), 0)
-                yw1 = max(int(y1 - margin * h), 0)
-                xw2 = min(int(x2 + margin * w), img_w - 1)
-                yw2 = min(int(y2 + margin * h), img_h - 1)
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    if len(detected) > 0:
+        for i, d in enumerate(detected):
+            x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+            xw1 = max(int(x1 - margin * w), 0)
+            yw1 = max(int(y1 - margin * h), 0)
+            xw2 = min(int(x2 + margin * w), img_w - 1)
+            yw2 = min(int(y2 + margin * h), img_h - 1)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
                 # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
+            faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
 
             # predict ages and genders of the detected faces
-            results = model.predict(faces)
-            predicted_genders = results[0]
-            ages = np.arange(0, 101).reshape(101, 1)
-            predicted_ages = results[1].dot(ages).flatten()
+        results = model.predict(faces)
+        predicted_genders = results[0]
+        ages = np.arange(0, 101).reshape(101, 1)
+        predicted_ages = results[1].dot(ages).flatten()
 
             # draw results
-            for i, d in enumerate(detected):
-                label = "{}, {}".format(int(predicted_ages[i]),
+        for i, d in enumerate(detected):
+            label = "{}, {}".format(int(predicted_ages[i]),
                                         "M" if predicted_genders[i][0] < 0.5 else "F")
-                draw_label(img, (d.left(), d.top()), label)
+            draw_label(img, (d.left(), d.top()), label)
 
-        cv2.imshow("result", img)
+        cv2.imwrite(os.path.join(IMAGE_DIR, "result.png"), img)
+        #out.write(img)
+        print("1 frame written")
         key = cv2.waitKey(-1) if image_dir else cv2.waitKey(30)
 
-        if key == 27:  # ESC
-            break
+    #if key == 27:  # ESC
+    #    break
 
 
 if __name__ == '__main__':
